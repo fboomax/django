c18c01e0462704a230fa24a1f13af2521417a68f
fboomax <fboomax@gmail.com>
2022-02-11 10:31:31 +0200

    Add manytomay and one to many models

--- MyProject/NewsApp/admin.py
+++ MyProject/NewsApp/admin.py
@@ -1,8 +1,12 @@
 from django.contrib import admin
-from .models import News, SportNews, RegistrationData
+from .models import News, SportNews, RegistrationData, Publication, Article, TheArticle, Reporter
 
 # Register your models here.
 
 admin.site.register(News)
 admin.site.register(SportNews)
 admin.site.register(RegistrationData)
+admin.site.register(Publication)
+admin.site.register(Article)
+admin.site.register(TheArticle)
+admin.site.register(Reporter)
--- MyProject/NewsApp/models.py
+++ MyProject/NewsApp/models.py
@@ -32,3 +32,46 @@ class RegistrationData(models.Model):
 
   def __str__(self):
     return self.username
+
+
+class Publication(models.Model):
+  title = models.CharField(max_length=20)
+
+  def __str__(self):
+    return self.title
+
+  class Meta: 
+    ordering = ("title",)
+
+
+class Article(models.Model):
+  headline = models.CharField(max_length=300)
+  publications = models.ManyToManyField(Publication)
+
+
+  def __str__(self):
+    return self.headline
+
+  class Meta: 
+    ordering = ("headline",)
+
+class Reporter(models.Model):
+  first_name = models.CharField(max_length=30)
+  last_name = models.CharField(max_length=30)
+  email = models.EmailField()
+
+  def __str__(self):
+    return self.first_name
+
+class TheArticle(models.Model):
+  headline = models.CharField(max_length=100)
+  pub_date = models.CharField(max_length=30)
+  reporter = models.ForeignKey(Reporter, on_delete=models.CASCADE)
+
+  def __str__(self):
+    return self.headline
+    
+
+  class Meta:
+    ordering = ('headline',)
+
